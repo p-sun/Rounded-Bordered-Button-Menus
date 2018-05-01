7646677657b66db7e12c984ce237f68ce5e8b331
Paige Sun <paige.sun.dev@gmail.com>
2018-05-01 13:36:19 -0400

    Left align all the tags.

--- RoundedButtonMenus/MenuCollection/MenuCollectionLayout.swift
+++ RoundedButtonMenus/MenuCollection/MenuCollectionLayout.swift
@@ -26,110 +26,20 @@ class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
         var maxY: CGFloat = -1.0
         attributes?.forEach { layoutAttribute in
             
-            print("layout attributes frame \(layoutAttribute.frame) current margin \(leftMargin) \(sectionInset.left)")
-
-            // If the current item i
+            // If THIS item is on the next row, reset its leftMargin
             if layoutAttribute.frame.origin.y >= maxY {
                 leftMargin = sectionInset.left
             }
-
+            // Get the maxY for all items
+            maxY = max(layoutAttribute.frame.maxY, maxY)
+            
+            // Set THIS item's origin.x to leftMargin. Note that the original origin.y is unchanged.
             layoutAttribute.frame.origin.x = leftMargin
 
+            // Calculate NEXT item's leftMargin by adding this item's width
             leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
-
-            maxY = max(layoutAttribute.frame.maxY, maxY)
-
-            print("new maxY \(maxY) left margin \(leftMargin)")
-
         }
         
         return attributes
     }
 }
-
-//class LeftAlignedFlowLayout: UICollectionViewLayout {
-//
-//    private var cache = [UICollectionViewLayoutAttributes]()
-//
-//    private var contentHeight: CGFloat = 0
-//    private var contentWidth: CGFloat {
-//        guard let collectionView = collectionView else {
-//            return 0
-//        }
-//        let insets = collectionView.contentInset
-//        return collectionView.bounds.width - (insets.left + insets.right)
-//    }
-//
-//
-//    let cellDim: CGFloat = 200
-//    let interItemSpacing: CGFloat = 10
-//
-//
-//    override var collectionViewContentSize: CGSize {
-//        return CGSize(width: contentWidth, height: contentHeight)
-//    }
-//
-//    override func prepare() {
-//        guard cache.isEmpty == true, let collectionView = collectionView else {
-//            return
-//        }
-//
-//        for item in 0 ..< collectionView.numberOfItems(inSection: 0) {
-//            //TODO: 1. Calulate Frame for each item
-//            let indexPath = IndexPath(item: item, section: 0)
-//
-//            let position = positionForIndex(index: indexPath.row)
-//
-//            let numberOfCellsInRow: CGFloat = position.row % 2 == 0 ? 3 : 2
-//            let spacingLeftAndRightOfCircles = contentWidth - (cellDim * numberOfCellsInRow) - (interItemSpacing * (numberOfCellsInRow - 1))
-//            let spacingLeftOfCircles = spacingLeftAndRightOfCircles / 2
-//            let xPosition = spacingLeftOfCircles + CGFloat(position.column) * (cellDim + interItemSpacing)
-//            let yPosition = cellDim * CGFloat(position.row)
-//
-//            let newFrame = CGRect(x: xPosition, y: yPosition, width: cellDim, height: cellDim)
-//
-//            let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
-//            attributes.frame = newFrame//CGRect(x: 0, y: 0, width: 300, height: 300) // Insert frame here
-//            cache.append(attributes)
-//
-//            //TODO: 2. Update Content Height
-//            contentHeight = max(contentHeight, CGFloat(position.row + 1) * cellDim)
-//        }
-//
-//    }
-//
-//    private struct Position {
-//        let row: Int
-//        let column: Int
-//    }
-//
-//    private func positionForIndex(index: Int) -> Position {
-//        let numberOfGroups = index / 5
-//        let itemPositionInGroup = index % 5
-//        let rowInGroup = itemPositionInGroup > 2 ? 1 : 0
-//        let row = numberOfGroups * 2 + rowInGroup
-//        let column = itemPositionInGroup % 3
-//        let pos = Position(row: row, column: column)
-//        print(pos)
-//        return pos
-//    }
-//
-//    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-//
-//        var visibleLayoutAttributes = [UICollectionViewLayoutAttributes]()
-//
-//        // Loop through the cache and look for items in the rect
-//        for attributes in cache {
-//            if attributes.frame.intersects(rect) {
-//                visibleLayoutAttributes.append(attributes)
-//            }
-//        }
-//        return visibleLayoutAttributes
-//    }
-//
-//    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-//        return cache[indexPath.item]
-//    }
-//
-//}
-//
